dcbb9fe2d78623d4e44aaf029c2a08940dd16fd6
aaronderen <aaronderendorf@icloud.com>
2024-11-09 20:51:55 -0800

    Misc Changes
    Gameplay Tuning:
    - Resized sturdy sprites. Collision shape changed to compensate
    
    - Tuned firerate progression (base CD .3; upgrade rate .05; min cooldown .05)
    
    - Changed starting player speed to 3 so it's slower than the fastest enemy (super) to encourage upgrading speed.
    
    ANIMATION:
    - Idle loop animation speed increases by a proportion of the player's move speed
    
    MISC
    - Added God Mode toggle to G key
    - Added game restart for testing to R key (sort of buggy cause it doesn't reset audio)
    
    ART:
    - Gave new weapon rig some materials
    - Added light to kitchen to test what it would look like.
    - Added temp directional light to world to see the gun better while working on anims.

--- objects/weapons/gumball_rig_base.gd
+++ objects/weapons/gumball_rig_base.gd
@@ -1,4 +1,6 @@
 extends Node3D
+
+@onready var player := get_tree().get_first_node_in_group("Player")
 @onready var animTree := $gumball_AnimationTree as AnimationTree
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +14,6 @@ func _process(delta: float) -> void:
 	
 func shoot():
 	animTree.set('parameters/OneShot/request', true)
+	
+func anim_idle_speed(float):
+	animTree.set("parameters/IdleLoopTimeScale/scale", float)
